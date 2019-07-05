83f276dddef8bf2926ff0e3b6880d9a3afff014a
Stanko Markovic <stankophp@gmail.com>
2019-07-05 12:46:01 +0300

    finished lesson 1

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,9 +1,12 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Thread;
+use App\Reply;
 use App\User;
-use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +26,27 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(Thread::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory('App\User')->create();
+        },
+        'title' => $faker->sentence,
+        'body' => $faker->paragraph,
+    ];
+});
+
+$factory->define(Reply::class, function (Faker $faker) {
+    return [
+        'user_id' => function () {
+            return factory('App\User')->create();
+        },
+        'thread_id' => function () {
+            return factory('App\Thread')->create();
+        },
+        'body' => $faker->paragraph,
     ];
 });
